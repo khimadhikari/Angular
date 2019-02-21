58b5b47f9fe6911230ca59cd0c1aafd5660c76d0
khimnadhikari <khimnadhikari@hotmail.com>
2019-02-22 04:45:32 +0530

    create employee form

--- employeeComponent/src/app/app.module.ts
+++ employeeComponent/src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './employee/create-employee.component';
 import { ListEmployeeComponent } from './employee/list-employee.component';
@@ -17,7 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
